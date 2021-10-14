5b6b5a60bb6bd2b0235c5c63d93b2dcb7fffb2ee
Uzi Refaeli <eluzix@gmail.com>
2021-10-14 13:25:47 +0300

    active_users_age

---
+++ reports/active_users_age.py
@@ -0,0 +1,52 @@
+import csv
+import time
+
+import boto3
+
+from clearvalue import app_config
+from clearvalue.analytics import is_internal_user, is_user_active, get_active_config
+from clearvalue.lib import utils
+from clearvalue.lib.dynamodb import ddb
+from clearvalue.lib.store import loaders, DBKeys
+
+
+def active_users(run_for=None, active_group=1):
+    tp1 = time.time()
+    if run_for is None:
+        stats_date = utils.date_to_str(utils.today())
+    else:
+        stats_date = run_for
+
+    active_group_config = get_active_config(active_group)
+    rows = []
+    for user in loaders.iter_users():
+        uid = user[DBKeys.HASH_KEY]
+        if is_internal_user(uid):
+            continue
+
+        if run_for is None:
+            daily_stats = ddb.get_item(app_config.resource_name('analytics'), DBKeys.hash_sort(uid, 'STATS'))
+        else:
+            daily_stats = ddb.get_item(app_config.resource_name('analytics'), DBKeys.hash_sort(uid, f'STATS:{run_for}'))
+
+        if daily_stats is None:
+            continue
+
+        if not is_user_active(daily_stats, active_group_config):
+            continue
+
+        rows.append([uid, str(daily_stats['user_age'])])
+
+    with open(f'active_users_age_{stats_date}.csv', 'w') as fout:
+        writer = csv.writer(fout)
+        writer.writerow(['UID', 'User Age'])
+        writer.writerows(rows)
+
+    print('All Done')
+
+
+if __name__ == '__main__':
+    boto3.setup_default_session(profile_name='clearvalue-sls')
+    app_config.set_stage('prod')
+
+    active_users(run_for='2021-09-14')
