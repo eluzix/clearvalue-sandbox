a8ea6fe00ced319dce654cd1683fe65fa619fcdb
Uzi Refaeli <eluzix@gmail.com>
2021-12-14 21:15:00 +0200

    graph optimization mid-work

--- gql_as.py
+++ gql_as.py
@@ -1,4 +1,5 @@
 import pprint
+import time
 
 import boto3
 
@@ -7,15 +8,15 @@ from utils import local_queries
 from utils.local_queries import securities_holdings_data, cash_type_info, pe_type_info, pe_account_info
 
 if __name__ == '__main__':
-    boto3.setup_default_session(profile_name='clearvalue-sls')
-    app_config.set_stage('prod')
+    # boto3.setup_default_session(profile_name='clearvalue-sls')
+    # app_config.set_stage('prod')
 
-    # boto3.setup_default_session(profile_name='clearvalue-stage-sls')
-    # app_config.set_stage('staging')
+    boto3.setup_default_session(profile_name='clearvalue-stage-sls')
+    app_config.set_stage('staging')
     # uid = '3e992797-4ab3-438b-977c-f6eb8b7ffcd5'
 
     # prod user
-    uid = 'e28e9038-c962-4e46-960e-c2b77a6468f0'
+    # uid = 'e28e9038-c962-4e46-960e-c2b77a6468f0'
 
     # demo account
     # uid = '4aaa981b-004b-4c39-a743-979ee062ddee'
@@ -30,7 +31,7 @@ if __name__ == '__main__':
     # uid = '7f58b698-59d7-42de-9c7d-a9e4e8d88573'
 
     # shaiazran@gmail in staging
-    # uid = '64ae0971-45aa-4e8f-8168-d55ffba09afa'
+    uid = '3e992797-4ab3-438b-977c-f6eb8b7ffcd5'
 
     # shai@claritus in prod
     # uid = '673e7d2c-4508-4658-9303-0646669d65f6'
@@ -69,10 +70,10 @@ if __name__ == '__main__':
     #                                                                        'endDate': '2021-03-23'
     #                                                                        })
 
-    ret = local_queries.securities_type_info(uid, tf={'timeFrame': '30days',
-                                                      'startDate': '2021-05-15',
-                                                      'endDate': '2021-06-16'
-                                                      })
+    # ret = local_queries.securities_type_info(uid, tf={'timeFrame': '30days',
+    #                                                   'startDate': '2021-05-15',
+    #                                                   'endDate': '2021-06-16'
+    #                                                   })
     # ret = local_queries.securities_account_info(uid, '61e0fade-b382-43a4-b6a8-15dc6f56fd96', tf={'timeFrame': '30days',
     #                                                                                              'startDate': '2021-07-01',
     #                                                                                              'endDate': '2021-07-31'
@@ -115,4 +116,12 @@ if __name__ == '__main__':
     # for ac in ret['loanTypeInfo']['accounts']:
     #     print(ac['account']['accountId'], '--', ac['account']['name'], '--', ac['account'].get('providerAccountId'))
 
+    tp1 = time.time()
+    ret = local_queries.all_assets_query(uid, tf={'timeFrame': 'max',
+                                                  'startDate': '2021-05-15',
+                                                  'endDate': '2021-06-16'
+                                                  })
+    tp2 = time.time()
+    print(f'-------------------------> {tp2-tp1}')
     pprint.pprint(ret)
+    print(f'-------------------------> {tp2-tp1}')
--- sandbox.py
+++ sandbox.py
@@ -1,9 +1,12 @@
 import datetime
+import time
 
+import boto3
 import pytz
 
+import cvutils
 from clearvalue import app_config
-from cvcore.store import DBKeys
+from cvcore.store import DBKeys, loaders
 from cvutils.dynamodb import ddb
 
 
@@ -29,11 +32,53 @@ def w():
         all_errors[error_type] = 'general error message here'
 
 
+def timing_test1():
+    uid = '3e992797-4ab3-438b-977c-f6eb8b7ffcd5'
+    account_id = 'ff12268d-1815-40d7-9e7f-617a2c14cf41'
+    account = ddb.get_item(app_config.resource_name('accounts'), DBKeys.user_account(uid, account_id))
+    start_date = '2020-10-21'
+    end_date = cvutils.date_to_str(cvutils.today())
+
+    tp1 = time.time()
+    graph_data = loaders.load_account_graph_data(account, 'custom', start_date, end_date)
+    tp2 = time.time()
+    print(f'[TEST 1] Loaded {len(graph_data)} in {tp2-tp1}')
+
+def timing_test2():
+    uid = '3e992797-4ab3-438b-977c-f6eb8b7ffcd5'
+    account_id = 'ff12268d-1815-40d7-9e7f-617a2c14cf41'
+    account = ddb.get_item(app_config.resource_name('accounts'), DBKeys.user_account(uid, account_id))
+    start_date = '2020-10-21'
+    end_date = cvutils.date_to_str(cvutils.today())
+
+    tp1 = time.time()
+    cur_date = cvutils.date_from_str(start_date) + datetime.timedelta(days=1)
+    keys = [DBKeys.hash_sort(account_id, DBKeys.account_time_point(start_date))]
+    run_till = cvutils.date_from_str(end_date)
+    while cur_date < run_till:
+        if cur_date.weekday() == 6:
+            keys.append(DBKeys.hash_sort(account_id, DBKeys.account_time_point(cur_date)))
+        # keys.append(DBKeys.hash_sort(account_id, DBKeys.account_time_point(cur_date)))
+        cur_date = cur_date + datetime.timedelta(days=1)
+    keys.append(DBKeys.hash_sort(account_id, DBKeys.account_time_point(end_date)))
+
+    # graph_data = loaders.load_account_graph_data(account, 'custom', start_date, end_date)
+    graph_data = ddb.batch_get_items(app_config.resource_name('accounts'), keys)
+
+    tp2 = time.time()
+    print(f'[TEST 2] Loaded {len(graph_data)} in {tp2-tp1}')
+
+
 if __name__ == '__main__':
     # boto_session = boto3.session.Session(profile_name='clearvalue-sls')
     # boto3.setup_default_session(profile_name='clearvalue-sls')
     # app_config.set_stage('prod')
 
-    ddb.collect_query_data = True
-    ret = ddb.get_item(app_config.resource_name('accounts'), DBKeys.info_key('f417d7b5-cb66-4ef1-a36a-9c6806d0af0f'))
+    boto3.setup_default_session(profile_name='clearvalue-stage-sls')
+    app_config.set_stage('staging')
+    timing_test1()
+    timing_test2()
+
+    # ddb.collect_query_data = True
+    # ret = ddb.get_item(app_config.resource_name('accounts'), DBKeys.info_key('f417d7b5-cb66-4ef1-a36a-9c6806d0af0f'))
     # print(ret)
